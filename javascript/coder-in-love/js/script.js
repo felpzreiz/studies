4164582b950781c33c77e7c45078455e168190d4
Felipe Reis <93730786+felpzreiz@users.noreply.github.com>
2024-04-29 14:53:05 -0300

    Update script.js

--- javascript/coder-in-love/js/script.js
+++ javascript/coder-in-love/js/script.js
@@ -1,1 +1,12 @@
+let i = 0;
+let message = "oi, quer sair comigo? ";
 
+typing();
+
+function typing(){
+  if (i < message.length){
+    document.getElementById('text').innerHTML += message.charAt(i);
+    i++;
+    setTimeout(typing, 100);
+  }
+}
