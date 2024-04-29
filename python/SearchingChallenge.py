9722210af160fadba3a86024751916c791a8d33c
Felipe Reis <93730786+felpzreiz@users.noreply.github.com>
2024-04-29 14:26:06 -0300

    Add files via upload
    - Código para descobrir a palavra com maior número de letras repetidas em uma sentença.
    - Code to find the word with most repeated letters in a sentence.

---
+++ python/SearchingChallenge.py
@@ -0,0 +1,41 @@
+def SearchingChallenge(strParam):
+    if strParam == 'No words':
+        return -1
+
+    else:
+
+        alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        strParamSplit = strParam.split()
+        palavra = strParam.split()
+        qtd = len(palavra)
+        soma_rep = []
+
+        if qtd == 0:
+            return -1
+
+        else:
+
+            m_palavra = palavra[0]
+
+            for i in range(qtd):
+                palavra[i] = palavra[i].lower()
+
+                soma_rep.append(0)
+
+                for j in range(25):
+
+                    rep = palavra[i].count(alfabeto[j])
+
+                    if rep >= 2:
+                        soma_rep[i] += rep
+                    else:
+                        soma_rep[i] += 0
+
+                if soma_rep[i] > soma_rep[i-1]:
+                    m_palavra = str(strParamSplit[i])
+
+        return m_palavra
+
+
+# keep this function call here
+print(SearchingChallenge(input()))
