4dee9e1f4cb165238f03e9588f02734e60d64213
Felipe Reis <93730786+felpzreiz@users.noreply.github.com>
2024-05-16 09:55:03 -0300

    Update app.js

--- javascript/estudo1/app.js
+++ javascript/estudo1/app.js
@@ -1,7 +1,7 @@
 import somar from "./soma.js"
 
-var num1 = Number(document.getElementById("num1").value)
-var num2 = Number(document.getElementById("num2").value)
+var num1 = Number(prompt("Insira o primeiro número: "))
+var num2 = Number(prompt("Insira o segundo número: "))
 var botao = document.querySelector("button")
 var resultado = document.getElementById("result")
 
