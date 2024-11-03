537248cc5f61bcf01f0a9c28a6138580e386f703
Felipe Reis <93730786+felpzreiz@users.noreply.github.com>
2024-11-03 17:30:45 -0300

    Update script.js

--- javascript/countdown/script.js
+++ javascript/countdown/script.js
@@ -1,4 +1,4 @@
-const dataFinal = new Date('October 13, 2024 00:00:00').getTime();
+const dataFinal = new Date('November 15, 2024 00:00:00').getTime();
 
 // Função para atualizar a contagem regressiva
 function atualizarContagemRegressiva() {
